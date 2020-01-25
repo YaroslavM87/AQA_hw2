1def61cf9bd713850de4337c0a466e1b789a4f03
Yaroslav Mzhelskikh <yarom87@mail.ru>
2020-01-25 16:05:54 +0700

    fix: calculation algorythm for CashbackHackService improved

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,21 +6,25 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashbackHackServiceTest {
 
     @Test
-    void shouldReturn550IfAmountIs450(){
-        CashbackHackService service = new CashbackHackService();
-        int amount = 450;
-        int actual = service.remain(amount);
-        int expected = 550;
-        assertEquals(actual, expected);
+    void shouldThrowExceptionIfAmountIsZeroOrLess() throws IllegalArgumentException {
+        assertThrows(IllegalArgumentException.class, () -> new CashbackHackService().remain(-1));
     }
 
     @Test
-    void shouldReturnZeroIfAmountDivisibleBy1000(){
+    void shouldReturnZeroIfAmountIsDivisibleBy1000(){
         CashbackHackService service = new CashbackHackService();
         int amount = 2000;
         int actual = service.remain(amount);
         int expected = 0;
         assertEquals(actual, expected);
     }
-    
+
+    @Test
+    void shouldReturnDifferenceBetweenBoundaryAndAmount(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 450;
+        int actual = service.remain(amount);
+        int expected = 550;
+        assertEquals(actual, expected);
+    }
 }
